cf6b60dac710d4008157fd04361466ce5777cf9b
m.shelepov <graf_ice@mail.ru>
2021-05-07 13:54:54 +0300

    Домашнее задание к уроку № 3 выполнено

---
+++ Java-2/HomeWork-3/src/Phonebook.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Phonebook {
+    private Map<String, String> data = new HashMap<>();
+
+
+    public void add(String surname, String phone){
+        data.put(phone, surname);
+    }
+    public void get(String surname){
+        Set<String> stringSet = new HashSet<>();
+        for (Map.Entry<String, String> entry : data.entrySet()){
+            if (Objects.equals(surname, entry.getValue())){
+                stringSet.add(entry.getKey());
+            }
+        }
+        System.out.println(stringSet);
+    }
+}
